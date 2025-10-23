3ae0f5bee75115f10fb66b7589af732f185a49bf
LouisCarlier5 <240772@umons.ac.be>
2025-10-23 10:30:04 +0000

    Importation

--- R/import.R
+++ R/import.R
@@ -1,15 +1,28 @@
 # Importation et remaniement des données
-
+birds<-read$csv("https://pasta.lternet.edu/package/data/eml/edi/1130/1/789d490f183f045e3b2ae9919eb6008e", cache_file="data/cache/birds.csv")
 
 # Etape 1 : Importation des données brutes --------------------------------
 
-
+thraupidae<-sfilter(birds, family_name == "Thraupidae")
 
 # Etape 2 : Description brève des données ---------------------------------
 
 
 
 # Etape 3 : Nettoyage des données  ----------------------------------------
+
+thraupidae$length[thraupidae$length == "-"] <- NA
+thraupidae$rwing[thraupidae$rwing == "-"] <- NA
+thraupidae$rtarsu[thraupidae$rtarsu == "-"] <- NA
+thraupidae$blenght[thraupidae$blenght == "-"] <- NA
+thraupidae$bheight[thraupidae$bheight == "-"] <- NA
+thraupidae$bwidth[thraupidae$bwidth == "-"] <- NA
+thraupidae$occiput[thraupidae$occiput == "-"] <- NA
+thraupidae$tail[thraupidae$tail == "-"] <- NA
+thraupidae$bmass[thraupidae$bmass == "-"] <- NA
+
+thraupidae$sex[thraupidae$sex == "I"] <- NA
+thraupidae$age[thraupidae$age == "I"] <- NA
 
 
 
@@ -18,4 +31,4 @@
 
 
 # Etape 5 : Sauvegarde locale des données retravaillées -------------------
-
+write$rds(thraupidae, "data/thraupidae.rds")
