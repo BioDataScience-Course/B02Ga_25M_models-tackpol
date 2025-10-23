23dc0880fd448fbf51be9132935ccc7e0eee1ba1
LouisCarlier5 <240772@umons.ac.be>
2025-10-23 13:47:20 +0000

    progression import des données

--- R/import.R
+++ R/import.R
@@ -24,11 +24,50 @@ thraupidae$bmass[thraupidae$bmass == "-"] <- NA
 thraupidae$sex[thraupidae$sex == "I"] <- NA
 thraupidae$age[thraupidae$age == "I"] <- NA
 
+thraupidae[, 6:14] <- lapply(thraupidae[, 6:14], as.numeric)
+
+thraupidae[, c(1:2, 4:5)] <- lapply(thraupidae[, c(1:2, 4:5)], as.factor)
+
+levels(thraupidae$sex)[levels(thraupidae$sex) == "F"] <- "Femelle"
+levels(thraupidae$sex)[levels(thraupidae$sex) == "M"] <- "Mâle"
+levels(thraupidae$age)[levels(thraupidae$age) == "A"] <- "Adulte"
+levels(thraupidae$age)[levels(thraupidae$age) == "J"] <- "Juvénile"
 
 
 # Etape 4 : Ajout des labels et des unités --------------------------------
-
+thraupidae<-labelise(thraupidae, label=list(
+  taxon_name="Espèce",
+  family_name="Famille",
+  date="Date de capture",
+  sex="Sexe",
+  age="Age",
+  length="Longueur",
+  rwing="Longueur de l'aile",
+  rtarsu="Longueur du tarse",
+  blenght="Longueur du bec",
+  bheight="Hauteur du bec",
+  bwidth="Largeur du bec",
+  occiput="Longueur de l'os occipital",
+  tail="Longueur de la queue",
+  bmass="Masse"),
+   units= list(
+  taxon_name=NA,
+  family_name=NA,
+  date=NA,
+  sex=NA,
+  age=NA,
+  length="cm",
+  rwing="cm",
+  rtarsu="cm",
+  blenght="cm",
+  bheight="cm",
+  bwidth="cm",
+  occiput="cm",
+  tail="cm",
+  bmass="g"
+))
 
 
 # Etape 5 : Sauvegarde locale des données retravaillées -------------------
 write$rds(thraupidae, "data/thraupidae.rds")
+
